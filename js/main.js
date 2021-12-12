863d01a02d101c0e8ae6c721779edf6abd9705b9
gecmanow <gecmanow@gmail.com>
2021-12-12 16:12:28 +0700

    add multi-button

--- js/main.js
+++ js/main.js
@@ -36,6 +36,94 @@ if(document.images) {
     arrow.src = "images/msg_arrow.gif";
 }
 
+let multiButton = document.getElementById('callback_multi-button');
+let multiButtonDrop = document.getElementById('callback_dropdown');
+let multiButtonDropItemTelegram = document.getElementById('callback_dropdown-item_telegram');
+let multiButtonDropItemWhatsapp = document.getElementById('callback_dropdown-item_whatsapp');
+let multiButtonDropItemPhone = document.getElementById('callback_dropdown-item_phone');
+let multiButtonTelegramIcon = document.getElementById('callback_telegram-icon');
+let multiButtonWhatsappIcon = document.getElementById('callback_whatsapp-icon');
+let multiButtonTelegramText = document.getElementById('callback_telegram-text');
+let multiButtonWhatsappText = document.getElementById('callback_whatsapp-text');
+let multiButtonPhoneText = document.getElementById('callback_phone-text');
+let multiButtonPhoneIcon = document.getElementById('callback_phone-icon');
+let multiButtonDropPhoneText = document.getElementById('callback_phone-text-mobile');
+let multiButtonDropPhoneIcon = document.getElementById('callback_phone-icon-mobile');
+let go = true;
+let arrD = ["fa fa-phone", "fab fa-telegram-plane", "fab fa-whatsapp"], i = -1;
+
+//interval = setInterval(change(), 1200);
+
+if (document.documentElement.clientWidth > 768) {
+    change();
+    multiButton.addEventListener('mouseover', function () {
+        multiButtonDrop.classList.remove('callback_stop');
+        multiButtonPhoneText.classList.remove('callback_stop');
+        go = false;
+        clearInterval(interval);
+        multiButtonPhoneIcon.className = 'fa fa-phone';
+    });
+    multiButton.addEventListener('mouseout', function () {
+        multiButtonDrop.classList.add('callback_stop');
+        multiButtonPhoneText.classList.add('callback_stop');
+        go = true;
+        change();
+    });
+} else {
+    change();
+    document.getElementById('callback_dropdown-item_phone-link').addEventListener('click', function (event) {
+        event.preventDefault();
+    });
+    multiButton.addEventListener('click', function () {
+        if((multiButtonDrop.classList.contains('callback_stop'))) {
+            multiButtonDrop.classList.remove('callback_stop');
+            multiButtonDropItemPhone.classList.remove('callback_stop');
+            go = false;
+            clearInterval(interval);
+            multiButtonPhoneIcon.className = 'fa fa-phone';
+        } else {
+            multiButtonDrop.classList.add('callback_stop');
+            multiButtonDropItemPhone.classList.add('callback_stop');
+            go = true;
+            change();
+        }
+    });
+}
+
+multiButtonDropItemTelegram.addEventListener('mouseover', function () {
+    multiButtonTelegramIcon.style.boxShadow = '0 0 5px rgba(0,0,0,0.5)';
+    multiButtonTelegramText.style.boxShadow = '0 0 5px rgba(0,0,0,0.5)';
+});
+multiButtonDropItemTelegram.addEventListener('mouseout', function () {
+    multiButtonTelegramIcon.style.boxShadow = 'none';
+    multiButtonTelegramText.style.boxShadow = 'none';
+});
+multiButtonDropItemWhatsapp.addEventListener('mouseover', function () {
+    multiButtonWhatsappIcon.style.boxShadow = '0 0 5px rgba(0,0,0,0.5)';
+    multiButtonWhatsappText.style.boxShadow = '0 0 5px rgba(0,0,0,0.5)';
+});
+multiButtonDropItemWhatsapp.addEventListener('mouseout', function () {
+    multiButtonWhatsappIcon.style.boxShadow = 'none';
+    multiButtonWhatsappText.style.boxShadow = 'none';
+});
+multiButtonDropItemPhone.addEventListener('mouseover', function () {
+    multiButtonDropPhoneIcon.style.boxShadow = '0 0 5px rgba(0,0,0,0.5)';
+    multiButtonDropPhoneText.style.boxShadow = '0 0 5px rgba(0,0,0,0.5)';
+});
+multiButtonDropItemPhone.addEventListener('mouseout', function () {
+    multiButtonDropPhoneIcon.style.boxShadow = 'none';
+    multiButtonDropPhoneText.style.boxShadow = 'none';
+});
+
+function change() {
+
+    let multiButtonPhoneIcon = document.getElementById('callback_phone-icon');
+    let arrD = ["fa fa-phone", "fab fa-telegram-plane", "fab fa-whatsapp"], i = -1;
+
+    interval = setInterval(function () {
+        multiButtonPhoneIcon.className = arrD[i = ++i == arrD.length ? 0 : i];
+    }, 1200)
+}
 
 const FULL_DASH_ARRAY = 283;
 const WARNING_THRESHOLD = 60;
@@ -60,32 +148,34 @@ let timePassed = 0;
 let timeLeft = TIME_LIMIT;
 let timerInterval = null;
 let remainingPathColor = COLOR_CODES.info.color;
+let timer = document.getElementById("timer");
 
-document.getElementById("timer").innerHTML = `
-<div class="base-timer">
-  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-    <g class="base-timer__circle">
-      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-      <path
-        id="base-timer-path-remaining"
-        stroke-dasharray="283"
-        class="base-timer__path-remaining ${remainingPathColor}"
-        d="
-          M 50, 50
-          m -45, 0
-          a 45,45 0 1,0 90,0
-          a 45,45 0 1,0 -90,0
-        "
-      ></path>
-    </g>
-  </svg>
-  <span id="base-timer-label" class="base-timer__label">${formatTime(
-    timeLeft
-)}</span>
-</div>
-`;
+if(timer) {
+timer.innerHTML = `
+    <div class="base-timer">
+      <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+        <g class="base-timer__circle">
+          <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+          <path
+            id="base-timer-path-remaining"
+            stroke-dasharray="283"
+            class="base-timer__path-remaining ${remainingPathColor}"
+            d="
+              M 50, 50
+              m -45, 0
+              a 45,45 0 1,0 90,0
+              a 45,45 0 1,0 -90,0
+            "
+          ></path>
+        </g>
+      </svg>
+      <span id="base-timer-label" class="base-timer__label">${formatTime(
+        timeLeft
+    )}</span>
+    </div>`;
 
-startTimer();
+    startTimer();
+}
 
 function onTimesUp() {
     clearInterval(timerInterval);
@@ -150,3 +240,6 @@ function setCircleDasharray() {
         .getElementById("base-timer-path-remaining")
         .setAttribute("stroke-dasharray", circleDasharray);
 }
+
+
+
--- resources/views/public/layout/home.blade.php
+++ resources/views/public/layout/home.blade.php
@@ -65,11 +65,41 @@ header("Cache-Control: max-age=0, public");
     </main>
     @include('public.inc.footer')
 </div>
+<div class="callback_multi-button" id="callback_multi-button">
+    <div class="callback_back">
+        <div class="callback_phone" id="callback_phone">
+            <a href="tel:79135396539" class="callback_dropdown-item_link" id="callback_dropdown-item_phone-link">
+                <i class="fa fa-phone" id="callback_phone-icon"></i>
+                <span id="callback_phone-text_back"><span class="callback_phone-text callback_stop" id="callback_phone-text">Позвонить</span></span>
+            </a>
+        </div>
+        <div class="callback_dropdown callback_stop" id="callback_dropdown">
+            <div class="callback_dropdown-item" id="callback_dropdown-item_telegram">
+                <a class="callback_dropdown-item_link" href="https://t.me/Alekstct" target="_blank">
+                    <i class="fab fa-telegram-plane" id="callback_telegram-icon"></i>
+                    <span class="callback_dropdown-item_text" id="callback_telegram-text">Написать в Telegram</span>
+                </a>
+            </div>
+            <div class="callback_dropdown-item" id="callback_dropdown-item_whatsapp">
+                <a class="callback_dropdown-item_link" href="https://wa.me/79135396539" target="_blank">
+                    <i class="fab fa-whatsapp" id="callback_whatsapp-icon"></i>
+                    <span class="callback_dropdown-item_text" id="callback_whatsapp-text">Написать в WhatsApp</span>
+                </a>
+            </div>
+            <div class="callback_dropdown-item callback_stop" id="callback_dropdown-item_phone">
+                <a class="callback_dropdown-item_link" href="tel:79135396539" target="_blank">
+                    <i class="fa fa-phone" id="callback_phone-icon-mobile"></i>
+                    <span class="callback_dropdown-item_text" id="callback_phone-text-mobile">Позвонить</span>
+                </a>
+            </div>
+        </div>
+    </div>
+</div>
 @include('public.inc.scripts')
-<link rel="stylesheet" href="https://cdn.envybox.io/widget/cbk.css">
-<script type="text/javascript" src="https://cdn.envybox.io/widget/cbk.js?wcb_code=d879a45c4408933937baf5a10afe2bc6" charset="UTF-8" async></script>
+
 <!--<a class="sticky-btn-link" href="tel:+79135396539">
     <span>Связаться с нами</span>
 </a>-->
+
 </body>
 </html>
