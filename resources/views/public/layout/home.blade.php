33e43a3033685f80326fdd60cf5c9b357de6a4e5
gecmanow <gecmanow@gmail.com>
2021-12-13 10:47:53 +0700

    fix

--- resources/views/public/layout/home.blade.php
+++ resources/views/public/layout/home.blade.php
@@ -68,26 +68,26 @@ header("Cache-Control: max-age=0, public");
 <div class="callback_multi-button" id="callback_multi-button">
     <div class="callback_back">
         <div class="callback_phone" id="callback_phone">
-            <a href="tel:79135396539" class="callback_dropdown-item_link" id="callback_dropdown-item_phone-link">
+            <a href="tel:+79135396539" class="callback_dropdown-item_link" id="callback_dropdown-item_phone-link">
                 <i class="fa fa-phone" id="callback_phone-icon"></i>
                 <span id="callback_phone-text_back"><span class="callback_phone-text callback_stop" id="callback_phone-text">Позвонить</span></span>
             </a>
         </div>
         <div class="callback_dropdown callback_stop" id="callback_dropdown">
             <div class="callback_dropdown-item" id="callback_dropdown-item_telegram">
-                <a class="callback_dropdown-item_link" href="https://t.me/Alekstct" target="_blank">
+                <a class="callback_dropdown-item_link" href="https://t.me/Alekstct">
                     <i class="fab fa-telegram-plane" id="callback_telegram-icon"></i>
                     <span class="callback_dropdown-item_text" id="callback_telegram-text">Написать в Telegram</span>
                 </a>
             </div>
             <div class="callback_dropdown-item" id="callback_dropdown-item_whatsapp">
-                <a class="callback_dropdown-item_link" href="https://wa.me/79135396539" target="_blank">
+                <a class="callback_dropdown-item_link" href="https://wa.me/79135396539">
                     <i class="fab fa-whatsapp" id="callback_whatsapp-icon"></i>
                     <span class="callback_dropdown-item_text" id="callback_whatsapp-text">Написать в WhatsApp</span>
                 </a>
             </div>
             <div class="callback_dropdown-item callback_stop" id="callback_dropdown-item_phone">
-                <a class="callback_dropdown-item_link" href="tel:79135396539" target="_blank">
+                <a class="callback_dropdown-item_link" href="tel:+79135396539">
                     <i class="fa fa-phone" id="callback_phone-icon-mobile"></i>
                     <span class="callback_dropdown-item_text" id="callback_phone-text-mobile">Позвонить</span>
                 </a>
